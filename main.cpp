1534c73092a1b0a9db3fffc8d8e739dc1660e373
Valeri Hristov <valeri.hristov@chaos.com>
2023-03-31 15:27:36 +0300

    Triangle ahoi

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdint>
 #include <fstream>
+#include <string>
 
 #include "src/out/ppm/PPMImageMeta.h"
 #include "src/out/ppm/PPMImage.h"
@@ -15,29 +16,30 @@ const int16_t RENDER_WIDTH = 640;
 const int16_t RENDER_HEIGHT = 480;
 const int16_t MAX_COLOR = 255;
 
-Color vis(Vec3 rayDir) {
-  rayDir = glm::abs(rayDir) * static_cast<float>(MAX_COLOR);
-  return Color((int32_t)rayDir.x, (int32_t)rayDir.y, (int32_t)rayDir.z);
-}
-
 int main() {
+  std::vector<Triangle> triangles;
   Triangle a(Vec3(-1.75, -1.75, -3), Vec3(1.75, -1.75, -3), Vec3(0, 1.75, -3));
-  std::cout << a.normal.x << ";" << a.normal.y << ";" << a.normal.z << std::endl;
 
-  std::cout << "The area is: " << a.area() << std::endl;
-    /*PPMImageMeta imageMetadata(RENDER_WIDTH, RENDER_HEIGHT, MAX_COLOR);
-    PPMImage image(imageMetadata);
-    Vec3 cameraPos;
-    Raygen s(RENDER_WIDTH, RENDER_HEIGHT, cameraPos, -1);
+  PPMImageMeta imageMetadata(RENDER_WIDTH, RENDER_HEIGHT, MAX_COLOR);
+  PPMImage image(imageMetadata);
+  Vec3 cameraPos{};
+  Raygen s(RENDER_WIDTH, RENDER_HEIGHT, cameraPos, -1);
 
-    for (int32_t row = 0; row < RENDER_HEIGHT; row++) {
-      for (int32_t col = 0; col < RENDER_WIDTH; col++) {
-        image.writePixel(vis(s.gen(col, row).dir));
+  for (int32_t row = 0; row < RENDER_HEIGHT; row++) {
+    for (int32_t col = 0; col < RENDER_WIDTH; col++) {
+      Ray& ray = s.gen(col, row);
+
+      if (a.intersect(ray)) {
+        image.writePixel(Color(255, 0, 0));
+      }
+      else {
+        image.writePixel(Color(0, 255, 0));
       }
     }
+  }
 
-    PPMImageFileWriter fileWriter(image, "out.ppm");
-    fileWriter.write();*/
+  PPMImageFileWriter fileWriter(image, "out.ppm");
+  fileWriter.write();
 
     return 0;
 }
--- src/utils/TypeDefs.h
+++ src/utils/TypeDefs.h
@@ -12,21 +12,39 @@ struct Ray {
   Vec3 dir;
 };
 
+using namespace glm;
+
 struct Triangle {
-  inline Triangle(const Vec3& a, const Vec3& b, const Vec3& c): a(a), b(b), c(c) {
-    normal = calculateNormal();
-  }
+  inline Triangle(const Vec3& a, const Vec3& b, const Vec3& c): a(a), b(b), c(c) {}
   Vec3 a;
   Vec3 b;
   Vec3 c;
-  Vec3 normal;
 
-  inline float area() {
+  inline float area() const {
     return glm::length(glm::cross(b - a, c - a)) / 2.0f;
   }
-private:
-  inline Vec3 calculateNormal() {
+
+  /* Calculates and returns the normal. It is intentional that it is not stored as a field of the struct. */
+  inline Vec3 normal() const {
     return glm::normalize(glm::cross(b - a, c - a));
+  }
+
+  inline bool intersect(const Ray& ray) const {
+    Vec3 n = normal();
+    float rayPlane = dot(ray.dir, n);
+
+    /* The ray is perpendicular or facing the back of the triangle */
+    if (rayPlane > 0.0f) {
+      return false;
+    }
+
+    float t = glm::dot(this->a - ray.origin, n) / rayPlane;
+    Vec3 p = ray.origin + t * ray.dir;
+    
+    // TODO: Compare with epsilon
+    return (dot(n, cross(b - a, p - a)) > 0.0f) &&
+      (dot(n, cross(c - b, p - b)) > 0.0f) &&
+      (dot(n, cross(a - c, p - c)) > 0.0f);
   }
 };
 
