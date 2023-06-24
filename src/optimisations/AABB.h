adb47f6a2916da7f71650c6487dea57f6cc546cf
quenchaman <valericfbg@gmail.com>
2023-06-24 23:12:29 +0300

    AABB ray box intersection WIP

--- src/optimisations/AABB.h
+++ src/optimisations/AABB.h
@@ -15,7 +15,7 @@ struct AABB {
   Vec3 min;
   Vec3 max;
 
-  void expand(const Triangle& triangle) {
+  inline void expand(const Triangle& triangle) {
     std::vector<Vertex> vertices = { triangle.a, triangle.b, triangle.c };
 
     for (Vertex& vertex : vertices) {
@@ -26,6 +26,36 @@ struct AABB {
         max = vertex.pos;
       }
     }
+  }
+
+  inline bool intersect(const Ray& ray) const {
+    // First, calculate the t values for the intersection of the ray with all planes
+    // with which the cube is defined.
+    
+    // Minimum point's x component t value
+    float tminx = (min.x - ray.origin.x) / ray.dir.x;
+    float tminy = (min.y - ray.origin.y) / ray.dir.y;
+    float tminz = (min.z - ray.origin.z) / ray.dir.z;
+
+    // Maximum component defined planes
+    float tmaxx = (max.x - ray.origin.x) / ray.dir.x;
+    float tmaxy = (max.y - ray.origin.y) / ray.dir.y;
+    float tmaxz = (max.z - ray.origin.z) / ray.dir.z;
+
+    // We need the 'greater' point between the intersections with the min-defined planes.
+    // Greater, in 2D, means more to the upper right direction.
+    float tmaxOfMin = tminx > tminy ? tminx : tminy;
+    tmaxOfMin = tminz > tmaxOfMin ? tminz : tmaxOfMin;
+
+    // Now for the maximum component we need the smaller point
+    float tminOfMax = tmaxx < tmaxy ? tmaxx : tmaxy;
+    tminOfMax = tmaxz < tminOfMax ? tmaxz : tminOfMax;
+
+    if (tminOfMax < tmaxOfMin) {
+      return false;
+    }
+
+    return true;
   }
 };
 
